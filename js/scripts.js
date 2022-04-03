1d9ebb8f145ea899579c3dc6767cf8c4e06cd2ad
RosarioRu <mariarruval@gmail.com>
2022-04-03 08:40:57 -0700

    Add functions to show and hide definition for next 3 terms.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
     $(".definition#java").toggle();
   });
 
+  $("#operators").click(function() {
+    $(".definition#operators").toggle();
+  });
+
+  $("#variables").click(function() {
+    $(".definition#variables").toggle();
+  });
+
+  $("#function").click(function() {
+    $(".definition#function").toggle();
+  });
+
 });
