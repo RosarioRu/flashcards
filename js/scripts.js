06a75169740a2bc4143f0e628b38c12915913046
RosarioRu <mariarruval@gmail.com>
2022-04-03 08:45:19 -0700

    Add ids and the functions to show/hide definition for last 4 terms.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,25 @@ $(document).ready(function() {
   $("#java").click(function() {
     $(".definition#java").toggle();
   });
-
   $("#operators").click(function() {
     $(".definition#operators").toggle();
   });
-
   $("#variables").click(function() {
     $(".definition#variables").toggle();
   });
-
   $("#function").click(function() {
     $(".definition#function").toggle();
   });
-
+  $("#methods").click(function() {
+    $(".definition#methods").toggle();
+  });
+  $("#arguments").click(function() {
+    $(".definition#arguments").toggle();
+  });
+  $("#parameters").click(function() {
+    $(".definition#parameters").toggle();
+  });
+  $("#jquery").click(function() {
+    $(".definition#jquery").toggle();
+  });
 });
