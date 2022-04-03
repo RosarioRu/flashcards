3cc33f570e90b2bad99418799964ac50b8d982d4
RosarioRu <mariarruval@gmail.com>
2022-04-02 17:49:13 -0700

    WIP: trying to get definition to show when card is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+  
   $("h3").click(function() {
-    $(".definition").show();
+    $("card").addClass("definition");
   });
+
+
+  // $("h3").click(function() {
+  //   $(".definition").show();
+  // });
+
 
 });
